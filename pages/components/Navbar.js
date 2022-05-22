71ee8475ddf452002c0b1f2cb8f148d5f79e85f5
MedNizar23 <mednizar.elouahmani@gmail.Com>
2022-05-22 15:39:50 +0100

    test

---
+++ pages/components/Navbar.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import {Disclosure} from '@headlesssui/react'
+import {MenuIcon, XIcon} from '@heroicons/react/outline'
+
+
+function Navbar() {
+  return (
+    <div>Navbar</div>
+  )
+}
+
+export default Navbar
